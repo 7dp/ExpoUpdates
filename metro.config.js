4e04952be76ee5a2b9c034c7d0968a64a9ebfae7
Wicak Radityo <wicakradityo778@gmail.com>
2023-10-19 16:20:56 +0700

    remove expo-updates (and their patch)

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
-// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const {mergeConfig} = require('@react-native/metro-config');
-const {getDefaultConfig} = require('expo/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// const {mergeConfig} = require('@react-native/metro-config');
+// const {getDefaultConfig} = require('expo/metro-config');
 
 /**
  * Metro configuration
